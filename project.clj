cafefaad085a3f9a02585de7319b12e2b35b8052
Sven Pedersen <sven.pedersen@crossroadsmusic.com>
2015-03-20 17:04:17 -0400

    add enlive + db args

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sventechie/friendui-mysql "0.0.2-SNAPSHOT"
+(defproject sventechie/friendui-mysql "0.0.3-SNAPSHOT"
   :description "A helper that implements friendui storage protocol for MySQL"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
@@ -8,6 +8,7 @@
                  [org.clojure/java.jdbc "0.3.6"]
                  [mysql/mysql-connector-java "5.1.34"]
                  [yesql "0.5.0-rc2"] ; db access
+                 [enlive "1.1.5"] ; html templating
                  [de.sveri/friendui "0.4.6"]]
   :profiles {:repl {:dependencies []}}
   :deploy-repositories [["clojars-self" {:url "https://clojars.org/repo"
--- src/sventechie/friendui_mysql/storage.clj
+++ src/sventechie/friendui_mysql/storage.clj
@@ -4,33 +4,34 @@
   (:gen-class))
 
 
-(defrecord FrienduiStorage []
+(defrecord FrienduiStorage [db-map]
   f-glob/FrienduiStorage
-  (account-activated? [this activation-id])
+  (account-activated? [this activation-id]
+    (db/account-activated? activation-id {:connection db-map}))
 
   (activate-account [this activation-id]
-    (db/activate-account activation-id))
+    (db/activate-account activation-id {:connection db-map}))
 
   (create-user [this email password role activation-id]
-    (db/create-new-user email password role activation-id))
+    (db/create-new-user email password role activation-id {:connection db-map}))
 
   (get-all-users [this]
-    (db/get-all-users))
+    (db/get-all-users {:connection db-map}))
 
   (get-user-for-activation-id [this id]
-    (db/get-user-for-activation-id id))
+    (db/get-user-for-activation-id id {:connection db-map}))
 
   (update-user [this username data-map]
-    (db/update-user username data-map))
+    (db/update-user username data-map {:connection db-map}))
 
   (username-exists? [this username]
-    (db/username-exists? username))
+    (db/username-exists? username {:connection db-map}))
 
   (get-loggedin-user-map [this]
-    (db/get-loggedin-user-map))
+    (db/get-loggedin-user-map {:connection db-map}))
 
   (get-old-pw-hash [this]
-    (db/get-old-pw-hash))
+    (db/get-old-pw-hash {:connection db-map}))
 
   (change-password [this new-pw]
-    (db/change-password new-pw)))
+    (db/change-password new-pw {:connection db-map})))
